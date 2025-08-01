2f7bb87688d9f05b2d45da76436ddd1e92810379
Jeel2901 <jeel.suthar@dcmail.ca>
2025-08-01 18:38:26 -0400

    lab 4 part 1 Update main.js
    Update variable and function part.

--- Lab 4/Part 1/main.js
+++ Lab 4/Part 1/main.js
@@ -1,13 +1,12 @@
-1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
-
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
   return array[random];
 }
+
 
 2. RAW TEXT STRINGS
 
