357808e34a5ead159b999ea6ad97b137b3792c06
Jeel2901 <jeel.suthar@dcmail.ca>
2025-08-01 19:34:25 -0400

    lab4 part2
    update main.js with using newImage , thumbBar , imageAlts .

--- Lab 4/Part 2/main.js
+++ Lab 4/Part 2/main.js
@@ -5,14 +5,30 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const imageAlts = {
+  'pic1.jpg': 'Closeup of a blue human eye',
+  'pic2.jpg': 'Rock that looks like a wave',
+  'pic3.jpg': 'Purple and white pansies',
+  'pic4.jpg': 'Section of wall from a pharaoh’s tomb',
+  'pic5.jpg': 'Butterfly on a leaf'
+};
 
 /* Looping through images */
+for (let i = 1; i <= 5; i++) {
+  const fileName = `pic${i}.jpg`;
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', 'images/' + fileName);
+  newImage.setAttribute('alt', imageAlts[fileName]);
+  thumbBar.appendChild(newImage);
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+  /* Add click event for each thumbnail */
+  newImage.addEventListener('click', function() {
+  displayedImage.setAttribute('src', 'images/' + fileName);
+  displayedImage.setAttribute('alt', imageAlts[fileName]);
+});
+}
 
 /* Wiring up the Darken/Lighten button */
