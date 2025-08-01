33262463db967ec092e1ae339238f4b5ce574181
Jeel2901 <jeel.suthar@dcmail.ca>
2025-08-01 19:02:18 -0400

    lab 4 part 1
    update index.html with script src , and in main.js , using const and newStory complete part1.

--- Lab 4/Part 1/main.js
+++ Lab 4/Part 1/main.js
@@ -14,23 +14,31 @@ const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 const insertY = ["the soup kitchen", "Disneyland", "the White House"];
 const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
-3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
-
 randomize.addEventListener('click', result);
 
 function result() {
+  let newStory = storyText;
 
-  if(customName.value !== '') {
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replaceAll(':insertx:', xItem);
+  newStory = newStory.replace(':inserty:', yItem);
+  newStory = newStory.replace(':insertz:', zItem);
+
+  if (customName.value !== '') {
     const name = customName.value;
-
+    newStory = newStory.replace('Bob', name);
   }
 
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-
+  if (document.getElementById('uk').checked) {
+    const weight = Math.round(300) + ' stone';
+    const temperature = Math.round(94) + ' centigrade';
+    newStory = newStory.replace('94 fahrenheit', temperature);
+    newStory = newStory.replace('300 pounds', weight);
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
